bc257788b17a142db16dda5c71823c6b3b7417d0
codeLocker <codeLockers@outlook.com>
2017-03-27 16:21:13 +0800

    完成ULWeatherDateView

--- ULife/View/Weather/ULWeatherDateView.swift
+++ ULife/View/Weather/ULWeatherDateView.swift
@@ -10,13 +10,57 @@ import UIKit
 
 class ULWeatherDateView: UIView {
 
+    fileprivate let dateLab : UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor.white
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        let date = Date()
+        label.text = date.ul_dateString(type: .chinese) + " " + date.ul_chineseLongWeekday()
+        return label
+    }()
+    
+    fileprivate let festivalLab : UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor.white
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        let date = Date()
+        let chineseYear = date.ul_chineseDate().year ?? ""
+        let chineseMonth = date.ul_chineseDate().month ?? ""
+        let chineseDay = date.ul_chineseDate().day ?? ""
+        var festival = date.ul_chineseFestival() ?? ""
+        let internationalFestival = date.ul_internationalFestival()
+        if internationalFestival != nil {
+            festival += internationalFestival!
+        }
+        label.text = chineseYear + chineseMonth + chineseDay + " " + festival
+        return label
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.backgroundColor = UIColor.red
+        loadUI()
+        layout()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
+    //MARK: - Load_UI
+    private func loadUI() {
+        self.addSubview(self.dateLab)
+        self.addSubview(self.festivalLab)
+    }
+    
+    private func layout() {
+        self.dateLab.snp.makeConstraints { (make) in
+            make.top.equalToSuperview()
+            make.left.equalToSuperview().offset(20)
+        }
+        self.festivalLab.snp.makeConstraints { (make) in
+            make.left.equalTo(dateLab)
+            make.bottom.equalToSuperview()
+        }
+    }
+    
 }
